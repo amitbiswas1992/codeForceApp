8fd583bd56d8a661cce06d90d9a72903bff4ccda
Amit Biswas <biswas1992@hotmail.com>
2020-04-23 01:40:34 -0400

    first commit

--- CodeforceRatingApp/ViewController.swift
+++ CodeforceRatingApp/ViewController.swift
@@ -9,12 +9,63 @@
 import UIKit
 
 class ViewController: UIViewController {
-
+    
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view.
+        
+        let url = "https://codeforces.com/api/blogEntry.comments?blogEntryId=79"
+        getData(from: url)
+        
     }
-
-
+    
+    private func getData(from url: String) {
+        
+        let task =   URLSession.shared.dataTask(with: URL(string: url)!, completionHandler: { data, response, error in
+            
+            guard let data = data , error == nil else {
+                print("Something went wrong")
+                return
+            }
+            //have data
+            
+            var result: JSONResponse?
+            do {
+                result = try JSONDecoder().decode(JSONResponse.self, from: data)
+            }
+            catch {
+                print ("failed to convert \(error.localizedDescription)")
+            }
+            
+            guard let json = result else {
+                return
+            }
+            
+            print(json.status)
+            print(json.result.first!)
+        })
+        
+        task.resume()
+    }
+    
+    
 }
+
+struct JSONResponse: Codable {
+    
+    var status: String
+    var result: [ResultModel]
+    
+}
+
+struct ResultModel: Codable {
+    
+    var id: Int
+    var creationTimeSeconds: Int
+    var commentatorHandle: String
+    var locale: String
+    var text: String
+    
+}
+
+
 
